567f6f278e819bf40d00b46d03b17c38212319c8
Ludoviko <me@ludoviko.xyz>
2022-01-15 18:13:36 +0000

    Initial Commit

---
+++ Ligitaj/LigitajApp.swift
@@ -0,0 +1,17 @@
+//
+//  LigitajApp.swift
+//  Ligitaj
+//
+//  Created by Louis Hollingworth on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LigitajApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
